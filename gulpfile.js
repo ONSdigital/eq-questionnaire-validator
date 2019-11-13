23940c45d433dccd779bf8a58bad7520a437d036
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2019-11-13 12:13:14 +0000

    Use prettier to format JSON (#173)
    * Use prettier to format JSON

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,23 @@
 const gulp = require("gulp");
-const beautify = require("gulp-jsbeautifier");
+const prettier = require("gulp-prettier");
 const diff = require("gulp-diff");
 const gutil = require("gulp-util");
 
-const beautifySettings = {
-    indent_size: 2, //eslint-disable-line camelcase
-    end_with_newline: true //eslint-disable-line camelcase
-
-};
-
-const formatPaths = [
-  "./schemas/**",
-  "./tests/schemas/**"
-];
+const formatPaths = ["./schemas/**", "./tests/schemas/**"];
 
 gulp.task("format", () =>
-  gulp.src(formatPaths, {base: "./"})
-    .pipe(beautify(beautifySettings))
+  gulp
+    .src(formatPaths, {
+      base: "./"
+    })
+    .pipe(prettier())
     .pipe(gulp.dest("."))
 );
 
 gulp.task("lint", () =>
-  gulp.src(formatPaths)
-    .pipe(beautify(beautifySettings))
+  gulp
+    .src(formatPaths)
+    .pipe(prettier())
     .pipe(diff())
     .pipe(
       diff.reporter({
@@ -31,7 +26,7 @@ gulp.task("lint", () =>
       })
     )
     .on("error", (err) => {
-    gutil.log("Linting failed try running `gulp format`");
-    throw err;
-  })
+      gutil.log("Linting failed try running `gulp format`");
+      throw err;
+    })
 );
