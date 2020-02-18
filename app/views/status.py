e257592555ac96f280f72c9693c5d6e62d6aeb40
Matthew Elcock <mattiose@outlook.com>
2020-02-18 10:23:19 +0000

    Improced /status response

--- app/views/status.py
+++ app/views/status.py
@@ -5,4 +5,4 @@ status_blueprint = Blueprint("status", __name__)
 
 @status_blueprint.route("/status", methods=["GET"])
 def validate_schema():
-    return Response(status=200)
+    return Response('OK')
