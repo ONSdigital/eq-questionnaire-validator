94ba1065ef3f3e7d4256eed089e31d6f89558fee
Ian Wootten <hi@niftydigits.com>
2020-05-27 17:30:59 +0100

    Break validation into separate method call

--- app/validators/answers/text_field_answer_validator.py
+++ app/validators/answers/text_field_answer_validator.py
@@ -9,10 +9,12 @@ class TextFieldAnswerValidator(AnswerValidator):
 
     def validate(self):
         super().validate()
+        self.validate_suggestions_url()
+        return self.errors
 
+    def validate_suggestions_url(self):
         if "suggestions_url" in self.answer and not self.is_suggestion_url_valid():
             self.add_error(self.INVALID_SUGGESTION_URL)
-        return self.errors
 
     def is_suggestion_url_valid(self):
         parsed_result = urlparse(self.answer["suggestions_url"])
