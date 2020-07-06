d9afa2c3852a5c80e144ad43f13decdf3698d004
Ian Wootten <hi@niftydigits.com>
2020-07-06 10:51:59 +0100

    Format error paths as pointer strings (#43)

--- tests/test_schema_validator.py
+++ tests/test_schema_validator.py
@@ -86,3 +86,14 @@ def test_invalid_survey_id_whitespace():
     validator.validate()
 
     assert validator.errors[0]["message"] == "'lms ' does not match '^[0-9a-z]+$'"
+
+
+def test_returns_pointer():
+    file = "schemas/invalid/test_invalid_survey_id_whitespace.json"
+    json_to_validate = _open_and_load_schema_file(file)
+
+    validator = SchemaValidator(json_to_validate)
+
+    validator.validate()
+
+    assert validator.errors[0]["pointer"] == "/survey_id"
