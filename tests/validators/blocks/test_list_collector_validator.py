70e6624e4d8b4f2d519bc7c8bbae2aeac561b42f
LJBabbage <lewis.babbage@ons.gov.uk>
2020-08-20 10:27:01 +0100

    Update Var name

--- tests/validators/blocks/test_list_collector_validator.py
+++ tests/validators/blocks/test_list_collector_validator.py
@@ -86,7 +86,10 @@ def test_invalid_list_collector_with_no_remove_answer_action():
     validator.validate()
 
     expected_errors = [
-        {"message": validator.NO_REDIRECT_TO_REMOVE_BLOCK, "block_id": "list-collector"}
+        {
+            "message": validator.NO_REMOVE_ANSWERS_FOR_LIST_ITEM,
+            "block_id": "list-collector",
+        }
     ]
 
     assert expected_errors == validator.errors
