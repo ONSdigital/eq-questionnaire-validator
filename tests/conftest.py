6a746574a9b3086770e60201e5a000b6fc918175
Andrew Maddaford <ajmaddaford@gmail.com>
2021-06-11 08:07:31 +0100

    Refactor duplicated json file finding code in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,26 +3,17 @@ import os
 
 def pytest_generate_tests(metafunc):
     if "valid_schema_filename" in metafunc.fixturenames:
-        valid_schema_filenames = all_valid_schema_files()
+        valid_schema_filenames = find_all_json_files("tests/schemas/valid")
         metafunc.parametrize("valid_schema_filename", valid_schema_filenames)
     if "rule_schema_filename" in metafunc.fixturenames:
-        rule_schema_filenames = all_rule_schema_files()
+        rule_schema_filenames = find_all_json_files("tests/schemas/rules")
         metafunc.parametrize("rule_schema_filename", rule_schema_filenames)
 
 
-def all_valid_schema_files():
-    schema_files = []
-    for folder, _, files in os.walk("tests/schemas/valid"):
-        for filename in files:
-            if filename.endswith(".json") and "invalid" not in filename:
-                schema_files.append(os.path.join(folder, filename))
-    return schema_files
-
-
-def all_rule_schema_files():
-    schema_files = []
-    for folder, _, files in os.walk("tests/schemas/rules"):
-        for filename in files:
-            if filename.endswith(".json"):
-                schema_files.append(os.path.join(folder, filename))
-    return schema_files
+def find_all_json_files(folder_name):
+    return [
+        os.path.join(folder, filename)
+        for folder, _, files in os.walk(folder_name)
+        for filename in files
+        if filename.endswith(".json")
+    ]
