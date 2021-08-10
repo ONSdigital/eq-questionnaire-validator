5b2ec4d7489b22538f6caef6d9e30213cba86d48
Mebin Abraham <mebin95+work@gmail.com>
2021-08-10 17:49:49 +0100

    Validate answer labels when multiple answers

--- app/validators/questions/calculated_question_validator.py
+++ app/validators/questions/calculated_question_validator.py
@@ -5,6 +5,7 @@ class CalculatedQuestionValidator(QuestionValidator):
     ANSWER_NOT_IN_QUESTION = "Answer does not exist within this question"
 
     def validate(self):
+        super().validate()
         self.validate_calculations()
         return self.errors
 
@@ -13,7 +14,7 @@ class CalculatedQuestionValidator(QuestionValidator):
         Validates that any answer ids within the 'answer_to_group'
         list are existing answers within the question
         """
-        answer_ids = [answer["id"] for answer in self.question.get("answers")]
+        answer_ids = [answer["id"] for answer in self.answers]
         for calculation in self.question.get("calculations"):
             for answer_id in calculation["answers_to_calculate"]:
                 if answer_id not in answer_ids:
--- app/validators/questions/question_validator.py
+++ app/validators/questions/question_validator.py
@@ -7,4 +7,22 @@ class QuestionValidator(Validator):
     def __init__(self, schema_element):
         super().__init__(schema_element)
         self.question = schema_element
+        self.answers = self.question.get("answers", [])
         self.context["question_id"] = schema_element["id"]
+
+    def validate(self):
+        if self.question["type"] != "MutuallyExclusive":
+            self._validate_answer_labels()
+
+        return self.errors
+
+    def _validate_answer_labels(self):
+        if len(self.answers) < 2:
+            return None
+
+        for answer in self.answers:
+            if not answer.get("label"):
+                self.add_error(
+                    "Answer label must be provided for questions with multiple answers",
+                    answer_id=answer["id"],
+                )
