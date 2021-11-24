d683a75616089c043f11a6612492de0a210c5f9e
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2021-11-24 15:51:57 +0000

    updated debug namespace

--- ajv/app.js
+++ ajv/app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const glob = require("glob");
 const express = require("express");
 const app = express();
-const debug = require("debug")("validator");
+const debug = require("debug")("ajv-schema-validator");
 
 const ajv = new Ajv({
   meta: false,
