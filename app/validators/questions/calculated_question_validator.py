2bf25066265e06d21461a5ad986b6d1a4f3a14dd
petechd <piotr.chudziak@ons.gov.uk>
2022-06-30 16:11:21 +0100

    Revert calc validation change

--- app/validators/questions/calculated_question_validator.py
+++ app/validators/questions/calculated_question_validator.py
@@ -6,4 +6,16 @@ class CalculatedQuestionValidator(QuestionValidator):
 
     def validate(self):
         super().validate()
+        self.validate_calculations()
         return self.errors
+
+    def validate_calculations(self):
+        """
+        Validates that any answer ids within the 'answer_to_group'
+        list are existing answers within the question
+        """
+        answer_ids = [answer["id"] for answer in self.answers]
+        for calculation in self.question.get("calculations"):
+            for answer_id in calculation["answers_to_calculate"]:
+                if answer_id not in answer_ids:
+                    self.add_error(self.ANSWER_NOT_IN_QUESTION, answer_id=answer_id)
