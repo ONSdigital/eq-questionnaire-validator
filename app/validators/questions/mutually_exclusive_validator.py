998e9bf3ea264d0610c4a77d1b5d4f1524920a71
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-07-14 15:53:10 +0100

    run make format

--- app/validators/questions/mutually_exclusive_validator.py
+++ app/validators/questions/mutually_exclusive_validator.py
@@ -1,6 +1,7 @@
+from app.answer_type import AnswerType
 from app.validators.questions.question_validator import QuestionValidator
 
-from app.answer_type import AnswerType
+
 class MutuallyExclusiveQuestionValidator(QuestionValidator):
     question = {}
     MUTUALLY_EXCLUSIVE_CONTAINS_MANDATORY = (
@@ -14,7 +15,10 @@ class MutuallyExclusiveQuestionValidator(QuestionValidator):
         if any(answer["mandatory"] is True for answer in self.answers):
             self.add_error(self.MUTUALLY_EXCLUSIVE_CONTAINS_MANDATORY)
 
-        if (AnswerType(self.answers[-1]["type"]) not in {AnswerType.CHECKBOX, AnswerType.RADIO}):
+        if AnswerType(self.answers[-1]["type"]) not in {
+            AnswerType.CHECKBOX,
+            AnswerType.RADIO,
+        }:
             self.add_error(
                 self.NON_CHECKBOX_RADIO_ANSWER, answer_id=self.answers[-1]["id"]
             )
