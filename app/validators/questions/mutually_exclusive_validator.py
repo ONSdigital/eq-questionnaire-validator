7e5de255147a2a60b8956393de596d6f994c3cdc
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-07-14 15:49:37 +0100

    add answertype enum and change if

--- app/validators/questions/mutually_exclusive_validator.py
+++ app/validators/questions/mutually_exclusive_validator.py
@@ -1,6 +1,6 @@
 from app.validators.questions.question_validator import QuestionValidator
 
-
+from app.answer_type import AnswerType
 class MutuallyExclusiveQuestionValidator(QuestionValidator):
     question = {}
     MUTUALLY_EXCLUSIVE_CONTAINS_MANDATORY = (
@@ -14,10 +14,8 @@ class MutuallyExclusiveQuestionValidator(QuestionValidator):
         if any(answer["mandatory"] is True for answer in self.answers):
             self.add_error(self.MUTUALLY_EXCLUSIVE_CONTAINS_MANDATORY)
 
-        if (
-            self.answers[-1]["type"] != "Checkbox"
-            and self.answers[-1]["type"] != "Radio"
-        ):
+        if (AnswerType(self.answers[-1]["type"]) not in {AnswerType.CHECKBOX, AnswerType.RADIO}):
+            print(len(self.answers[-1]))
             self.add_error(
                 self.NON_CHECKBOX_RADIO_ANSWER, answer_id=self.answers[-1]["id"]
             )
