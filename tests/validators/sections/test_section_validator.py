8f7dbe8f4877663024c13f249989a88a25d5a5d3
petechd <piotr.chudziak@ons.gov.uk>
2022-12-08 10:34:21 +0000

    Add invalid schema and tests

--- tests/validators/sections/test_section_validator.py
+++ tests/validators/sections/test_section_validator.py
@@ -21,3 +21,33 @@ def test_invalid_list_reference_in_custom_summary():
     validator.validate()
 
     assert validator.errors == expected_errors
+
+
+def test_invalid_section_summary_items():
+    filename = "schemas/invalid/test_invalid_list_collector_section_summary_items.json"
+    questionnaire_schema = QuestionnaireSchema(_open_and_load_schema_file(filename))
+    section = questionnaire_schema.get_section("section-companies")
+    validator = SectionValidator(section, questionnaire_schema)
+
+    expected_errors = [
+        {
+            "id": "any-companies-or-branches-answer",
+            "message": "related_answers id not present in any list collector",
+            "section_id": "section-companies",
+        },
+        {
+            "id": "any-companies-or-branches-answer",
+            "message": "Item anchor answer id not present in any list collector",
+            "section_id": "section-companies",
+        },
+        {
+            "id": "any-companies-or-branches-answer",
+            "message": "No label found for any-companies-or-branches-answer, only "
+            "answers that support labels can be used as related answers",
+            "section_id": "section-companies",
+        },
+    ]
+
+    validator.validate()
+
+    assert validator.errors == expected_errors
