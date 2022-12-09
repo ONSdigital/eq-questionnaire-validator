fef9c8e6265b67cfdb768e499ef8f5e295aa5f63
petechd <53475968+petechd@users.noreply.github.com>
2022-12-09 14:14:13 +0000

    Update app/error_messages.py
    Co-authored-by: Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>

--- app/error_messages.py
+++ app/error_messages.py
@@ -8,7 +8,7 @@ RELATED_ANSWERS_NOT_PRESENT = "Related_answers field not present for summary"
 RELATED_ANSWERS_NOT_IN_LIST_COLLECTOR = (
     "Related_answers id not present in any list collector"
 )
-NO_LABEL_FOR_RELATED_ANSWER = "No label found for {answer_id}, only answers that support labels can be used as related answers"
+NO_LABEL_FOR_RELATED_ANSWER = "No label found for answer '{answer_id}', only answers that support labels can be used as related answers"
 ITEM_ANCHOR_ANSWER_ID_NOT_IN_LIST_COLLECTOR = (
     "Item anchor answer id not present in any list collector"
 )
