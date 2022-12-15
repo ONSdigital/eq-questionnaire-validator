8c4989bfed112c4419f973141038ffe879464317
Rhys Berrow <rhys.berrow@ons.gov.uk>
2022-12-15 13:18:40 +0000

    Lint

--- app/validators/questionnaire_schema.py
+++ app/validators/questionnaire_schema.py
@@ -444,4 +444,8 @@ class QuestionnaireSchema:
         value_sources = get_object_containing_key(
             block["calculation"]["operation"], "source"
         )
-        return [source[1]["identifier"] for source in value_sources if source[0] == "answers"]
+        return [
+            source[1]["identifier"]
+            for source in value_sources
+            if source[0] == "answers"
+        ]
