b7ebd0285b50abdaa16b14e32c2ddfc40c9fece3
Guilhem Forey <guilhemforey@Administrators-MacBook-Pro.local>
2023-01-31 09:32:02 +0000

    Remove dead debugging code

--- app/validators/schema_validator.py
+++ app/validators/schema_validator.py
@@ -32,11 +32,7 @@ class SchemaValidator(Validator):
         ]:
             for filename in glob.glob(glob_path):
                 with open(filename, encoding="utf8") as schema_file:
-                    try:
-                        json_data = load(schema_file)
-                    except Exception as e:
-                        content = schema_file.read()
-                        print(content)
+                    json_data = load(schema_file)
                     store[json_data["$id"]] = json_data
         return store
 
