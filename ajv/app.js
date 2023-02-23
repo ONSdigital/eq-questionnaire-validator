da0b286ffc3f68f5af718e684d3fe875e5aced7e
Amit Sinha <amit@creativeappdevs.com>
2023-02-23 17:35:58 +0000

    Fixing AJV validator working issue

--- ajv/app.js
+++ ajv/app.js
@@ -10,8 +10,8 @@ const app = express();
 
 const ajValidator = new Ajv2020({
   allErrors: false,
-  strict: true,
-  strictSchema: false, // to avoid key errors as a part of strict being true. https://ajv.js.org/options.html#strictschema
+  strict: true, // With strict on we are getting issues with $ref example "Error: strict mode: missing type "string" for keyword "pattern""
+  strictSchema: false, // https://ajv.js.org/options.html#strictschema
 });
 
 app.use(
@@ -24,17 +24,22 @@ app.listen(5002, () => {
   debug("Server running on port 5002");
 });
 
+// Export our app for testing purposes
+export default app;
+
 app.get("/status", (req, res, next) => {
   return res.sendStatus(200);
 });
 
 glob("schemas/**/*.json", (er, schemas) => {
   schemas.forEach((currentSchema) => {
-    const data = fs.readFileSync(currentSchema); // eslint-disable-line security/detect-non-literal-fs-filename
-    ajValidator.addSchema(JSON.parse(data));
+    if (!(currentSchema.valueOf() === "schemas/questionnaire_v1.json")) {
+      const data = fs.readFileSync(currentSchema); // eslint-disable-line security/detect-non-literal-fs-filename
+      ajValidator.addSchema(JSON.parse(data)).compile(true);
+    }
   });
   const baseSchema = fs.readFileSync("schemas/questionnaire_v1.json");
-  const validate = ajValidator.compile(baseSchema);
+  const validate = ajValidator.compile(JSON.parse(baseSchema));
 
   app.post("/validate", (req, res, next) => {
     debug("Validating questionnaire: " + req.body.title);
@@ -43,7 +48,7 @@ glob("schemas/**/*.json", (er, schemas) => {
       return res.json({
         success: false,
         errors: validate.errors.sort((errorA, errorB) => {
-          return errorA.dataPath.length - errorB.dataPath.length;
+          return errorA.instancePath.length - errorB.instancePath.length;
         }),
       });
     }
---
+++ ajv/tests/test_questionnaire_validator.js
@@ -0,0 +1,128 @@
+// Import the dependencies for testing
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../app";
+import fs from "fs";
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("AJV schema Validator", () => {
+  describe("GET /validate", () => {
+    it("test_invalid_question_description", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/invalid/test_invalid_question_description.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          const failure = res.body.errors.pop();
+          res.body.should.have.property("success", false);
+          failure.should.have.property("keyword", "enum");
+          failure.should.have.property(
+            "message",
+            "must be equal to one of the allowed values"
+          );
+          done();
+        });
+    });
+    it("test_valid_question_description", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/valid/test_question_description.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          res.body.should.not.have.property("success", false);
+          done();
+        });
+    });
+    it("test_valid_placeholder", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/valid/test_placeholder_full.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          res.body.should.not.have.property("success", false);
+          done();
+        });
+    });
+    it("test_invalid_placeholder", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/invalid/test_invalid_placeholder_full.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          res.body.should.have.property("success", false);
+          const failure = res.body.errors.pop();
+          failure.should.have.property("keyword", "const");
+          failure.should.have.property("message", "must be equal to constant");
+          done();
+        });
+    });
+    it("test_valid_new_routing_and", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/valid/test_new_routing_and.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          res.body.should.not.have.property("success", false);
+          done();
+        });
+    });
+    it("test_invalid_new_routing_and", (done) => {
+      const data = fs.readFileSync(
+        "ajv/tests/schemas/invalid/test_invalid_new_routing_and.json"
+      );
+      chai
+        .request(app)
+        .post("/validate")
+        .set("content-type", "application/json")
+        .send(JSON.parse(data))
+        .end((err, res) => {
+          isError(err);
+          res.should.have.status(200);
+          res.body.should.have.property("success", false);
+          const failure = res.body.errors.pop();
+          failure.should.have.property("keyword", "oneOf");
+          failure.should.have.property(
+            "message",
+            "must match exactly one schema in oneOf"
+          );
+          done();
+        });
+    });
+  });
+});
+function isError(err) {
+  if (err) {
+    throw new Error("Unable to execute test case");
+  }
+}
