f87f038be8a4311ecd89ab7f6dbb413fe205305c
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2023-05-02 17:45:17 +0100

    fix min number max number constants

--- tests/validators/answers/test_number_answer_validator.py
+++ tests/validators/answers/test_number_answer_validator.py
@@ -13,8 +13,8 @@ def test_minimum_value():
         "id": "answer-4",
         "label": "Max/Min out of system limits",
         "mandatory": False,
-        "maximum": {"value": MAX_NUMBER},
-        "minimum": {"value": MIN_NUMBER},
+        "maximum": {"value": 9_999_999_999_999_999},
+        "minimum": {"value": -999_999_999_999_999},
         "type": "Number",
     }
 
@@ -24,15 +24,15 @@ def test_minimum_value():
 
     assert validator.errors[0] == {
         "message": validator.MINIMUM_LESS_THAN_LIMIT,
-        "value": MIN_NUMBER,
-        "limit": -99_999_999_999_999,
+        "value": -999_999_999_999_999,
+        "limit": MIN_NUMBER,
         "answer_id": "answer-4",
     }
 
     assert validator.errors[1] == {
         "message": validator.MAXIMUM_GREATER_THAN_LIMIT,
-        "value": MAX_NUMBER,
-        "limit": 999_999_999_999_999,
+        "value": 9_999_999_999_999_999,
+        "limit": MAX_NUMBER,
         "answer_id": "answer-4",
     }
 
