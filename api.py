f9fde4ee2a84f676cc975342e6a55e50560e5bcb
petechd <petechadwick1944@gmail.com>
2024-09-09 15:14:36 +0100

    Refactor remaining method

--- api.py
+++ api.py
@@ -4,17 +4,12 @@ import urllib
 from json import JSONDecodeError
 
 import requests
-
+import uvicorn
+from fastapi import Body, FastAPI, Response
 from requests import RequestException
 from structlog import get_logger
 
 from app.validators.questionnaire_validator import QuestionnaireValidator
-
-from fastapi import FastAPI, Response, Body
-
-from requests import request
-
-import uvicorn
 
 app = FastAPI()
 
@@ -22,7 +17,6 @@ app = FastAPI()
 @app.get("/status")
 async def validate_schema():
     return Response(status_code=200)
-
 
 
 logger = get_logger()
@@ -39,33 +33,37 @@ async def validate_schema_request_body(payload=Body(None)):
 
 
 @app.get("/validate")
-def validate_schema_from_url():
-    values = request.args
-    if "url" in values:
-        logger.info("Validating schema from URL", url=values["url"])
+async def validate_schema_from_url(url: str = ""):
+    if url:
+        logger.info("Validating schema from URL", url=url)
         try:
-            with urllib.request.urlopen(values["url"]) as url:
-                return validate_schema(url.read().decode())
+            with urllib.request.urlopen(url) as url:
+                return await validate_schema(data=url.read().decode())
         except urllib.error.URLError:
             return (
                 Response(
-                    status=404,
-                    response=f'Could not load schema at URL [{values["url"]}]',
+                    status_code=404,
+                    content=f"Could not load schema at URL [{url}]",
                 ),
             )
 
 
 async def validate_schema(data):
-    # try:
-    #     json_to_validate = json.loads(data)
-    # except JSONDecodeError:
-    #     logger.info("Could not parse JSON", status=400)
-    #     return Response(status=400, response="Could not parse JSON")
+    json_to_validate = None
+    if data:
+        if isinstance(data, str):
+            try:
+                json_to_validate = json.loads(data)
+            except JSONDecodeError:
+                logger.info("Could not parse JSON", status=400)
+                return Response(status_code=400, content="Could not parse JSON")
+        elif isinstance(data, dict):
+            json_to_validate = data
 
     response = {}
     try:
         ajv_response = requests.post(
-            AJV_VALIDATOR_URL, json=data, timeout=10
+            AJV_VALIDATOR_URL, json=json_to_validate, timeout=10
         )
         ajv_response_dict = ajv_response.json()
 
@@ -78,25 +76,19 @@ async def validate_schema(data):
         logger.info("AJV Schema validator service unavailable")
         return json.dumps(error="AJV Schema validator service unavailable")
 
-    validator = QuestionnaireValidator(data)
+    validator = QuestionnaireValidator(json_to_validate)
     validator.validate()
 
     if len(validator.errors) > 0:
         response["errors"] = validator.errors
         logger.info("Questionnaire validator returned errors", status=400)
-        response = Response(
-            content=json.dumps(response),
-            status_code=400
-        )
+        response = Response(content=json.dumps(response), status_code=400)
 
         return response
 
     logger.info("Schema validation passed", status=200)
 
-    response = Response(
-        content=json.dumps(response),
-        status_code=200
-    )
+    response = Response(content=json.dumps(response), status_code=200)
 
     return response
 
