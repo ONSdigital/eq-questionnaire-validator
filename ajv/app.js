851407206008f3cf573db364348ef9d46d3a3d62
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-03-31 11:37:38 +0100

    Update app.js for PORT

--- ajv/app.js
+++ ajv/app.js
@@ -6,6 +6,7 @@ import Debug from "debug";
 
 const debug = Debug("ajv-schema-validator");
 const app = express();
+const PORT = process.env.PORT || 5002
 
 app.use(
   express.json({
@@ -13,8 +14,8 @@ app.use(
   })
 );
 
-app.listen(5002, () => {
-  debug("Server running on port 5002");
+app.listen(PORT, () => {
+  debug(`Server running on port  ${PORT}`);
 });
 
 const ajValidator = new Ajv2020({
