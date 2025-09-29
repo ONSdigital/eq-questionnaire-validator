93d4019447ab3237190998f63d07a5bc15a4bb65
oms09 <omar.moulana@ons.gov.uk>
2025-09-29 14:54:12 +0100

    removed enum for const

--- ajv/tests/test_questionnaire_validator.js
+++ ajv/tests/test_questionnaire_validator.js
@@ -38,10 +38,10 @@ describe("AJV schema Validator", () => {
           res.should.have.status(200);
           const failure = res.body.errors.pop();
           res.body.should.have.property("success", false);
-          failure.should.have.property("keyword", "enum");
+          failure.should.have.property("keyword", "const");
           failure.should.have.property(
             "message",
-            "must be equal to one of the allowed values"
+            "must be equal to constant"
           );
           done();
         });
