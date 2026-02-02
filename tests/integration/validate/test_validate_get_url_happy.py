387ef4ac2dc72088b108a4d93b8e0ca3b763a9b6
oms09 <omar.moulana@ons.gov.uk>
2026-02-02 15:59:53 +0000

    valid url

---
+++ tests/integration/validate/test_validate_get_url_happy.py
@@ -0,0 +1,11 @@
+from urllib.parse import urlencode
+
+def test_validate_get_url_happy_path(client, mock_urlopen_valid, mock_ajv_valid):
+
+    url = "https://raw.githubusercontent.com/ONSdigital/eq-questionnaire-runner/main/schemas/test/en/test_checkbox.json"
+    response = client.get("/validate", params={"url": url})
+
+    assert response.status_code == 200
+    assert response.json() == {}
+    assert "errors" not in response.json()
+
